d6d7a90107e3b0872cae2767ac25961a5d190b06
Patrick Malouin <patrick.malouin@auth0.com>
2021-12-10 14:50:06 -0500

    fix(put): keep full bucket when already full

--- lib/put.lua
+++ lib/put.lua
@@ -6,19 +6,22 @@ local current_time = redis.call('TIME')
 local current_timestamp_ms = current_time[1] * 1000 + current_time[2] / 1000
 
 local current = redis.call('HMGET', KEYS[1], 'r')
+local new_content
 
 if current[1] then
-  tokens_to_add = math.min(current[1] + tokens_to_add, bucket_size)
+  new_content = math.min(current[1] + tokens_to_add, bucket_size)
+else
+  new_content = math.min(bucket_size + tokens_to_add, bucket_size)
 end
 
 redis.replicate_commands()
-if tokens_to_add < bucket_size then
+if new_content < bucket_size then
   redis.call('HMSET', KEYS[1],
             'd', current_timestamp_ms,
-            'r', tokens_to_add)
+            'r', new_content)
   redis.call('EXPIRE', KEYS[1], ttl)
 else
   redis.call('DEL', KEYS[1])
 end
 
-return { tokens_to_add, current_timestamp_ms }
+return { new_content, current_timestamp_ms }
